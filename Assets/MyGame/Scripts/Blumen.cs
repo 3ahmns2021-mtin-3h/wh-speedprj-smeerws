8492bb369a543d8655c989d6a71a8c62519e7fa9
smeerws <hello@smeerws.at>
2021-05-10 13:01:56 +0200

    timer

--- Assets/MyGame/Scripts/Blumen.cs
+++ Assets/MyGame/Scripts/Blumen.cs
@@ -6,6 +6,7 @@ public class Blumen : MonoBehaviour
 {
     private int spacePress;
     private int kPress;
+    private bool isSpacePressed, isKPressed;
 
     public float startTime, stopTime, timer;
     bool isTimerRunning;
@@ -16,18 +17,44 @@ public class Blumen : MonoBehaviour
         spacePress = 0;
         kPress = 0;
         isTimerRunning = false;
+        isSpacePressed = isKPressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         timer = stopTime + (Time.time - startTime);
-        Debug.Log("Overall" + timer);
+        int seconds = (int)timer % 60;
+
+        //Debug.Log("Overall " + timer + " sec " + seconds);
+
+        //Methode die StopTimer ausführt für Space Press; 
+        if (isTimerRunning)
+        {
+            if (isSpacePressed && seconds >= 1)
+            {
+                isSpacePressed = false;
+                TimerStop();
+                TimerReset();
+                // Blumen machen;
+            }
+            if(isKPressed && seconds >= 1)
+            {
+                isKPressed = false;
+                TimerStop();
+                TimerReset();
+                //Methoden Call
+            }
+        }
+
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             spacePress++;
             Debug.Log("Space pressed " + spacePress);
+            isSpacePressed = true;
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
@@ -41,22 +68,31 @@ public class Blumen : MonoBehaviour
             kPress++;
             Debug.Log("K pressed " + kPress);
         }
+
+        
     }
 
     void TimerStart()
-    {
-        if (isTimerRunning)
+    {  
+        if (!isTimerRunning)
         {
             startTime = Time.time;
+            isTimerRunning = true;
+
+            Debug.Log("in Start Time " + startTime);
         }
-       
+
     }
 
     void TimerStop()
     {
+        Debug.Log("in Stop Timer");
         if (isTimerRunning)
         {
             stopTime = Time.time;
+            isTimerRunning = false;
+            Debug.Log("in Stop Timer " + stopTime);
+
         }
        
     }
@@ -67,6 +103,15 @@ public class Blumen : MonoBehaviour
         
         timer = 0.0f;
         startTime = 0.0f;
-        stopTime = 0.0f; 
+        stopTime = 0.0f;
+        Debug.Log("Timer Reset");
     }
+
+    public void CreateBlumen(int numberBlumen)
+    {
+        for(int i = 0; i < numberBlumen; i++)
+        {
+            Instantiate();
+        }
+    } 
 }
